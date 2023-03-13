5b3749ce5b180b1e3ea9f94d3c27d39768e48958
el mehdi belfkih <mehdibm.az@gmail.com>
2023-03-13 06:49:24 +0100

    ..

--- mandatory/code/pipex_utils.c
+++ mandatory/code/pipex_utils.c
@@ -6,7 +6,7 @@
 /*   By: ebelfkih <ebelfkih@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 10:58:58 by ebelfkih          #+#    #+#             */
-/*   Updated: 2023/03/13 05:59:24 by ebelfkih         ###   ########.fr       */
+/*   Updated: 2023/03/13 06:46:38 by ebelfkih         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,17 @@ void	exit_message(int i, t_vars *vars)
 	message[2] = ft_strdup("pipe error !!");
 	message[3] = ft_strdup(" command not found !!");
 	message[4] = ft_strdup(" execve error !!");
-	message[5] = NULL;
+	message[5] = ft_strdup("There is no input file !!");
+	message[6] = NULL;
 	write (vars->trm, message[i], strlen(message[i]));
-	ft_clear (message, 5);
+	if (i == 1 || i == 2 || i == 3 || i == 4)
+	{
+		ft_clear (vars->path, 10000);
+		ft_clear (vars->cmds, 10000);
+		free(vars->input);
+		free(vars->output);
+	}
+	ft_clear (message, 6);
 	exit(1);
 }
 
@@ -33,8 +41,10 @@ void	my_fopen(t_vars *vars)
 {
 	vars->fd_in = open(vars->input, O_RDWR, 0777);
 	vars->fd_out = open(vars->output, O_RDWR | O_CREAT | O_TRUNC, 0777);
-	if (vars->fd_out == -1 || vars->fd_in == -1)
+	if (vars->fd_out == -1)
 		exit_message (0, vars);
+	if (vars->fd_in == -1)
+		exit_message (5, vars);
 	dup2(vars->fd_in, 0);
 	dup2(vars->fd_out, 1);
 	close(vars->fd_in);
